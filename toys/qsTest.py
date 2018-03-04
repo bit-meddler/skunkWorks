af3c2f859ae62dad9b5cff3d3eeda9dfa1f88b48
RichH <RichH@beacon-optical.co.uk>
2018-03-04 18:03:29 +0000

    Off the top of my head

---
+++ toys/qsTest.py
@@ -0,0 +1,32 @@
+"""
+Toy quicksort implementation
+"""
+
+def part( array, lo_idx, hi_idx ):
+    idx = lo_idx - 1
+    pivot = (array[ lo_idx ] + array[ hi_idx ]) / 2
+    
+    for i in range( lo_idx, hi_idx ):
+        if( array[i] <= pivot ):
+            idx += 1
+            # swap
+            array[ i ], array[ idx ] = array[ idx ], array[ i ]
+        twoPointPrint( array, i, idx )
+    return idx
+    
+    
+def quicksort( array, lo_idx, hi_idx ):
+    p_idx = part( array, lo_idx, hi_idx )
+    
+    quicksort( array, lo_idx,  p_idx  )
+    quicksort( array, p_idx+1, hi_idx )
+    
+    
+def twoPointPrint( array, top_i, bot_i ):
+    print( " " + ("   "*top_i) + "v" )
+    print( array )
+    print( " " + ("   "*bot_i) + "^" )
+    
+if( __name__ == "__main__" ):
+    l = [2, 4, 7, 3, 5, 9, 8, 2, 3, 5, 3 ]
+    quicksort( l, 0, len(l)-1 )
