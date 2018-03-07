75174a38cf3fa6ec6feb2a68871c3429db606260
RichH <RichH@beacon-optical.co.uk>
2018-03-07 11:41:29 +0000

    Implementation without recursion (keep my own task stack)

--- toys/qsTest.py
+++ toys/qsTest.py
@@ -12,7 +12,7 @@ def part( array, lo_idx, hi_idx ):
             idx += 1
             # swap
             array[ i ], array[ idx ] = array[ idx ], array[ i ]
-        twoPointPrint( array, i, idx )
+        #twoPointPrint( array, i, idx )
     idx += 1
     array[ hi_idx ], array[ idx ] = array[ idx ], array[ hi_idx ]
     return idx
@@ -24,13 +24,27 @@ def quicksort( array, lo_idx, hi_idx ):
         
         quicksort( array, lo_idx,  p_idx-1 )
         quicksort( array, p_idx+1, hi_idx  )
+
+
+def noRecurse( array ):
+    q = [ [0, len( array )-1], ]
+    while( len( q ) > 0 ):
+        lo, hi = q.pop()
+        if( lo < hi ):
+            pi = part( array, lo, hi )
+            q.append( [lo, pi-1] )
+            q.append( [pi+1, hi] )
     
     
 def twoPointPrint( array, top_i, bot_i ):
     print( " " + ("   "*top_i) + "v" )
     print( array )
     print( " " + ("   "*bot_i) + "^" )
+
     
 if( __name__ == "__main__" ):
-    l = [2, 4, 7, 3, 5, 9, 8, 2, 3, 5, 3 ]
+    l = [ 2, 4, 7, 3, 5, 9, 8, 2, 3, 5, 3 ]
     quicksort( l, 0, len(l)-1 )
+    l = [ 2, 4, 7, 3, 5, 9, 8, 2, 3, 5, 3 ]
+    print( "Non Recursive" )
+    noRecurse( l )
