2916a109c4171c357e7f96828702386f578b81db
RichH <RichH@beacon-optical.co.uk>
2018-03-08 13:31:04 +0000

    Composing L-mats

--- motionFiles/htrParse.py
+++ motionFiles/htrParse.py
@@ -7,6 +7,10 @@ from animTypes import SkeletonData, AnimData
 
 
 MY_NAMES = [ ".tx", ".ty", ".tz", ".rx", ".ry", ".rz" ]
+ROT_CONVERT = {
+    "Degrees": lambda x: x,
+    "Radians": np.radians,
+}
 
 def _dfs( graph, root ):
     path = []
@@ -18,6 +22,30 @@ def _dfs( graph, root ):
             q = graph[ leaf ] + q
     return path
     
+    
+def _formRot( rx, ry, rz, order="XYZ" ):
+    X = np.eye( 3 )
+    Y = np.eye( 3 )
+    Z = np.eye( 3 )
+    M = np.eye( 3 )
+    
+    cx, sx = np.cos( rx ), np.sin( rx )
+    cy, sy = np.cos( ry ), np.sin( ry )
+    cz, sz = np.cos( rz ), np.sin( rz )
+    
+    # Post multiplying
+    order.reverse()
+    for ax in order:
+        if ax=="X":
+            M *= X
+        if ax=="Y":
+            M *= Y
+        if ax=="Z":
+            M *= Z
+            
+    return M
+    
+    
 def readHTR( file_name ):
 
     fh = open( file_name, "r" )
@@ -28,6 +56,12 @@ def readHTR( file_name ):
     skel = SkeletonData()
     
     line = lines.pop().strip()
+    header_rot_order = "XYZ"
+    header_rot_units = "Degrees"
+    header_rate = -1
+    header_frames = -1
+    header_joints = -1
+    
     while( True ):
         
         if( line == "" ):
@@ -38,17 +72,18 @@ def readHTR( file_name ):
             while( True ):
                 line = lines.pop().strip()
                 if( "NumSegments" in line:
-                    pass # 36
+                     _, header_joints = line.split()
                 if( "NumFrames" in line:
-                    pass #  120
+                     _, header_frames = line.split()
                 if( "DataFrameRate" in line:
-                    pass #  120
+                     _, header_rate = line.split()
                 if( "EulerRotationOrder" in line:
-                    pass #  ZYX
+                    _, header_rot_order = line.split()
+                    header_rot_order.reverse() # Assuming this is reversed!
                 if( "CalibrationUnits" in line:
                     pass #  mm
                 if( "RotationUnits" in line:
-                    pass #  Degrees
+                    _, header_rot_units = line.split()
                 if( "GlobalAxisofGravity" in line:
                     pass #  Y
                 if( "BoneLengthAxis" in line:
@@ -73,9 +108,10 @@ def readHTR( file_name ):
                     s_graph[ child ] = []
                 rev_LUT[ child ] = parent
                 
-            root = s_graph[ "GLOBAL" ][0] # assume one root
+            root = s_graph[ "GLOBAL" ][0] # assume one root!
             skel.joint_names = _dfs( s_graph, root )
             skel.joint_count = len( skel.joint_names )
+            
             # make LUT
             for i, name in enumerate( skel.joint_names ):
                 skel.joint_LUT[ name ] = i
@@ -91,12 +127,26 @@ def readHTR( file_name ):
                 self.joint_parents[ skel.joint_LUT[ joint ] ] = p_idx
                 
         if( "BasePosition" in line ):
-            # name, 0, 0, 0, r?, r?, r?, 1
-            #
+            sken.joint_L_mats = np.zeros( (skel.joint_count,3,4), dtype=np.float32 )
+            while( True ):
+                line = lines.pop().strip()
+                if( "[" in line ):
+                    break
+                # get base pose
+                toks = line.split()
+                tx, ty, tz, rx, ry, rz, _ = map( float, toks[1:] )
+                j_id = skel.joint_LUT[ toks[0] ]
+                # Form L mats
+                rx = ROT_CONVERT[ header_rot_units ]( rx )
+                ry = ROT_CONVERT[ header_rot_units ]( ry )
+                rz = ROT_CONVERT[ header_rot_units ]( rz )
+                M = _formRot( rx, ry, rz, header_rot_order )
+                skel.joints_L_mats[ j_id, :3, :3 ] = M
+                skel.joints_L_mats[ j_id, :,   3 ] = [ tx, ty, tz ]
             # Last Hierarchy section
             break
             
-    # Tidy up skeleton? BFS to generate evaluate order
+    # Tidy up skeleton?
     
     line = lines.pop().strip()
     while( len( lines ) > 0 ):
