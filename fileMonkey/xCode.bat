a9ee7575e2809ff9ff56ea7f684331d4c3f9e923
RichH <RichH@beacon-optical.co.uk>
2018-05-29 15:56:08 +0100

    Minor improvement

--- fileMonkey/xCode.bat
+++ fileMonkey/xCode.bat
@@ -1,3 +1,9 @@
-ECHO "Transcoding '%1' to '%2'"
+ECHO OFF
+ECHO "Transcoding '%1' to h264 in a '%2'"
+
 SET FFMPEG_PATH="C:\port\ffmpeg\bin\ffmpeg.exe"
-FOR /F "tokens=*" %%G IN ('dir /b %1') DO %FFMPEG_PATH% -i "%%G" -c:v libx264 -preset slow -crf 22 -c:a copy "%%~nG%2"
+SET FFMPEG_CMD="-c:v libx264 -preset slow -crf 22 -c:a copy"
+
+FOR /F "tokens=*" %%G IN ('dir /b %1') DO (
+    %FFMPEG_PATH% -i "%%G" %FFMPEG_CMD% "%%~nG%2"
+)
