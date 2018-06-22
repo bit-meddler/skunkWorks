9f24027fc4b944b3f95bdedf6b5764ea566b0f2f
RichH <RichH@beacon-optical.co.uk>
2018-06-22 09:45:20 +0100

    tuning Scene detection

--- fileMonkey/extractNstamp.bat
+++ fileMonkey/extractNstamp.bat
@@ -1,9 +1,7 @@
-ECHO OFF
+ECHO ON
 ECHO "Extracting Feature points from '%1'..."
 
 SET FFMPEG_PATH="C:\port\ffmpeg\bin\ffmpeg.exe"
 
-%FFMPEG_PATH% -i "%1" -filter:v "select='eq(pict_type\,I)" ^
--vf "drawtext=fontfile='C\:\\Windows\\Fonts\\Consolas.ttf':text='\%%H:\%%M:\%%S':^
-fontcolor=0xFFFFFFFF:fontsize=12^
-x=10:h-th-10:" -vsync vfr "%1_th_%%02d.jpg"
+%FFMPEG_PATH% -i "%1" -vf select=eq(pict_type\,I),drawtext=fontfile='C\:\\Windows\\Fonts\\Consola.ttf':text='%%{pts\:hms}':fontcolor=white:fontsize=22:x=10:y=h-th-10 -vsync vfr "%~n1_if_%%02d.jpg"
+%FFMPEG_PATH% -i "%1" -vf select=gt(scene\,0.31),drawtext=fontfile='C\:\\Windows\\Fonts\\Consola.ttf':text='%%{pts\:hms}':fontcolor=white:fontsize=22:x=10:y=h-th-10 -vsync vfr "%~n1_sd_%%02d.jpg"
